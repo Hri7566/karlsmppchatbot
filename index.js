50e8fb81651927db46e611dfb0ca317150c969c3
Marty Robbins <yee@haw.com>
2019-02-19 19:09:44 -0500

    Yee haw

---
+++ index.js
@@ -0,0 +1,22 @@
+// Import Client.js
+const MPPClient = require("./Client.js");
+
+// Creation of the bot that will connect to MPP
+client = new MPPClient('ws://multiplayerpiano.com', undefined);
+
+// Start of the bot
+client.start();
+
+// When the bot is connected...
+client.on("hi", () => {
+	// Log a message in the console
+	console.log("Online");
+
+	// Go into a channel
+	client.setChannel("whats up I'm bored");
+
+	// Showing that the bot is ready
+	setTimeout(() => {
+		client.sendArray([{ m:'a', message:"Connected" }]);
+	}, 100)
+});
