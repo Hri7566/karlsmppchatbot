6326eedc151fc37f2b923e0997dc788f79705fab
Marty Robbins <yee@haw.com>
2019-02-20 21:54:37 -0500

    fdds

--- Tempclient.js
+++ Tempclient.js
@@ -1,15 +1,18 @@
 const MPPClient = require("./client.js");
 
+const msgroom = require("./index.js")
+
 var temporaryclient = new MPPClient('ws://multiplayerpiano.com', undefined);
 
 temporaryclient.start();
+
 
  
 setTimeout(() => {
         client.sendArray([{ m:'userset', set:{name:"Ṃᾄʀẋ☭Bot ❤"} }]);
     }, 100);
 
-    (function msgroom (room, msgss) {
+ (function msgroom (room, msgss) {
     var room = args[1];
     var msgss = msgs[1];
     temporaryclient.setChannel(room);
