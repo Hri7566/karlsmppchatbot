3235e7a9353dc431493a89798ae5c3bff0ad0987
Hri7566 <hri7566@gmail.com>
2019-04-20 17:39:15 -0400

    Fixed Bot It's vacation _id

--- index.js
+++ index.js
@@ -89,7 +89,8 @@ function sendchat(string){
 
 var Kings = [client.getOwnParticipant()._id,
              "0236f354fc5685c5bd18f152", // Karl Marx
-             "470ba9eaa66583fb5ee24f67" // hri
+             "29587fe40f6bd85fc757cc2d", // hri
+             "99da8b2e9735a34da6d2ac35 " // Temporary bop it - Remove after he gets home
             ]
 
 var Nobles = [
